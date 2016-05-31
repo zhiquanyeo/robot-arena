6d7e69bdfb7af28a41233f579aa1e7315e57281e
Zhiquan Yeo <zyeo8@bloomberg.net>
2016-05-31 18:18:59 -0400

    Support latching mode lighted hardware button

--- lighted-hardware-button.js
+++ lighted-hardware-button.js
@@ -7,29 +7,50 @@ class LightedHardwareButton extends HardwareButton {
 	constructor(config) {
 		super(config);
 
+		this.d_lightActiveState = GPIO.HIGH;
+		this.d_latchMode = false;
+		// Single, non latching
 		if (config.lightPin !== undefined) {
-			this.d_lightActiveState = GPIO.HIGH;
-			if (config.lightActiveState !== undefined) {
-				this.d_lightActiveState = config.lightActiveState;
-			}
-
 			this.d_lightHW = new GPIO.DigitalOutput(config.lightPin);
+		}
+		else if (config.latching && 
+				 config.latching.on !== undefined &&
+				 config.latching.off !== undefined) {
+			this.d_lightOnHW = new GPIO.DigitalOutput(config.latching.on);
+			this.d_lightOffHW = new GPIO.DigitalOutput(config.latching.off);
+			this.d_latchMode = true;
 		}
 		else {
 			throw new Error('Light Pin is required');
 		}
+
+		if (config.lightActiveState !== undefined) {
+			this.d_lightActiveState = config.lightActiveState;
+		}
 	}
 
 	setLight(val) {
-		if (val) {
-			this.d_lightHW.write(this.d_lightActiveState);
-		}
-		else {
-			if (this.d_lightActiveState === GPIO.HIGH) {
-				this.d_lightHW.write(GPIO.LOW);
+		if (this.d_latchMode) {
+			if (val) {
+				this.d_lightOnHW.write(this.d_lightActiveState);
+				this.d_lightOffHW.write(!this.d_lightActiveState);
 			}
 			else {
-				this.d_lightHW.write(GPIO.HIGH);
+				this.d_lightOnHW.write(!this.d_lightActiveState);
+				this.d_lightOffHW.write(this.d_lightActiveState);
+			}
+		}
+		else {
+			if (val) {
+				this.d_lightHW.write(this.d_lightActiveState);
+			}
+			else {
+				if (this.d_lightActiveState === GPIO.HIGH) {
+					this.d_lightHW.write(GPIO.LOW);
+				}
+				else {
+					this.d_lightHW.write(GPIO.HIGH);
+				}
 			}
 		}
 	}
