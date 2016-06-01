1efdadfad8f7a26fb936a048d77a4a63503298b6
Zhiquan Yeo <zyeo8@bloomberg.net>
2016-06-01 10:07:04 -0400

    Fleshed out arena FSM

--- arena.js
+++ arena.js
@@ -10,7 +10,6 @@ const Stately = require('stately.js');
 var STATE_IDLE = 'idle';
 var STATE_PARTIAL_READY = 'partial-ready';
 var STATE_FULLY_READY = 'fully-ready';
-var STATE_MATCH_START = 'match-start';
 var STATE_MATCH_IN_PROGRESS = 'match-in-progress';
 var STATE_MATCH_END = 'match-end';
 
@@ -28,8 +27,9 @@ class Arena extends EventEmitter {
 		this.d_matchClock = new MatchClock(this.d_i2c);
 
 		this.d_currentState = STATE_IDLE;
-		this.d_matchTime = 0;
 		this.d_matchTimerToken = null;
+		this.d_currClock = 0;
+		this.d_currClockStart = 0;
 
 		this.d_arenaState = {
 			playerStations: {},
@@ -44,6 +44,11 @@ class Arena extends EventEmitter {
 			throw new Error('Match Button configuration is required');
 		}
 
+		this.d_matchTime = 180; // seconds
+		if (config.matchTime !== undefined) {
+			this.d_matchTime = config.matchTime;
+		}
+
 		this.d_matchButton = new LightedHardwareButton(config.matchButton);
 
 		if (config.playerStations) {
@@ -56,6 +61,56 @@ class Arena extends EventEmitter {
 		}
 
 		var _arenaThis = this;
+
+		// Utility function for reset
+		var _arenaReset = function() {
+			for (var pId in _arenaThis.d_players) {
+				var ps = _arenaThis.d_players[pId];
+				ps.reset();
+			}
+
+			if (_arenaThis.d_matchTimerToken) {
+				clearInterval(_arenaThis.d_matchTimerToken);
+				_arenaThis.d_matchTimerToken = undefined;
+			}
+		};
+
+		// Utility function for getting arena ready for match
+		var _setArenaReady = function (fsm) {
+			// Set up the button
+			_arenaThis.d_matchButton.setLight(true);
+
+			// When the button is pressed, we start the match
+			_arenaThis.d_matchButton.once('pressed', function () {
+				_arenaThis.d_matchButton.setLight(false);
+				fsm.setMachineState(STATE_MATCH_IN_PROGRESS);
+
+				if (_arenaThis.d_matchTimerToken) {
+					clearInterval(_arenaThis.d_matchTimerToken);
+					_arenaThis.d_matchTimerToken = undefined;
+				}
+
+				_arenaThis.d_currClockStart = (new Date()).getTime();
+				_arenaThis.d_currClock = _arenaThis.d_currClockStart;
+				var matchTimeMs = _arenaThis.d_matchTime * 1000;
+				// Start the clock
+				_arenaThis.d_matchTimerToken = setInterval(function () {
+					_arenaThis.d_currClock = (new Date()).getTime();
+					var elapsed = _arenaThis.d_currClock - _arenaThis.d_currClockStart;
+					var matchTimeLeftMs = matchTimeMs - elapsed;
+
+					if (matchTimeLeftMs > 0) {
+						_arenaThis.d_matchClock.setTime(matchTimeLeftMs / 1000);
+					}
+					else {
+						_arenaThis.d_matchClock.setTime(0);
+						clearInterval(_arenaThis.d_matchTimerToken);
+						fsm.setMachineState(STATE_MATCH_END);
+					}
+				}, 10);
+			});
+		};
+
 		this.d_fsm = Stately.machine({
 			'idle': {
 				ready: function () {
@@ -67,11 +122,12 @@ class Arena extends EventEmitter {
 						psCount--;
 						if (psCount === 0) {
 							_actionThis.setMachineState(STATE_FULLY_READY);
+							_setArenaReady(_actionThis);
 						}
 					};
 
-					for (var pId in this.d_players) {
-						var ps = this.d_players[pId];
+					for (var pId in _arenaThis.d_players) {
+						var ps = _arenaThis.d_players[pId];
 						ps.once('ready', _checkReadyFunc);
 						ps.setReady();
 					}
@@ -80,22 +136,23 @@ class Arena extends EventEmitter {
 			},
 			'partial-ready': {
 				reset: function () {
-
+					_arenaReset();
 				}
 			},
 			'fully-ready': {
 				reset: function () {
-					
+					_arenaReset();
 				}
 			},
-			'match-start': {
-
-			},
 			'match-in-progress': {
-
+				reset: function () {
+					_arenaReset();
+				}
 			},
 			'match-end': {
-
+				reset: function () {
+					_arenaReset();
+				}
 			}
 		});
 
